ea30b75d6e383c37edcff51b7d29e9f0f404c225
Christian Samuel <113886171+christooo2811@users.noreply.github.com>
2022-09-23 10:49:13 +0700

    Add files via upload

---
+++ contoh1.py
@@ -0,0 +1,6 @@
+name = input("Siapa namamu?")
+print(f"Hello, {name}")
+bil1 = int( input("Masukkan bilangan pertama: ") )
+bil2 = int("Masukkan bilangan kedua         : ") )
+jumlah = bil1 + bil2
+print(f"Hasil penjumlahan {bil1} dan {bil2} adalah {jumlah}") 
